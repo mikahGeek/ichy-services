b513cd08217748c0551d974e8e2eab9473d7d04b
osboxes.org <mikah.geek@gmail.com>
2020-08-25 14:27:23 -0400

    init, pre-integration

---
+++ src/Requests/Request.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Requests
+{
+    public class Request
+    {
+        public DateTime RequestDate { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
---
+++ test/TestRequests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestRequests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
